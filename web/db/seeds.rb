75b1b63a9e15bd9189e8f90ee538ecf1620a5e95
Louie Yap <LouieY@magenic.com>
2018-09-24 21:09:11 +0800

    Added users page with edit credentials and delete. Added restriction also so that only admin user can access this page.

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+encryptor = EncryptPasswordService.new
+@password = encryptor.encrypt('admin', '123')
+User.create!(username: 'admin', password: @password, admin: true, person: Person.create(firstname: 'admin'))
